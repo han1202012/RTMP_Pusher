be4dbc964be276e2d30cb3bcbc5ba0eaf480a46a
octopus <octopus_truth@163.com>
2020-06-11 09:58:18 +0800

    update

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,10 +1,14 @@
 #include <jni.h>
 #include <string>
+#include "librtmp/rtmp.h"
+#include <x264.h>
 
 extern "C" JNIEXPORT jstring JNICALL
 Java_kim_hsl_rtmp_MainActivity_stringFromJNI(
         JNIEnv* env,
         jobject /* this */) {
     std::string hello = "Hello from C++";
+    RTMP_Alloc();
+    x264_picture_t *p = new x264_picture_t;
     return env->NewStringUTF(hello.c_str());
 }
--- app/src/main/java/kim/hsl/rtmp/MainActivity.java
+++ app/src/main/java/kim/hsl/rtmp/MainActivity.java
@@ -8,7 +8,6 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    // Used to load the 'native-lib' library on application startup.
     static {
         System.loadLibrary("native-lib");
     }
