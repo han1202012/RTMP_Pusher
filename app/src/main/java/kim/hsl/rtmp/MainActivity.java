54617597b1b4e17d4815a28503365911cf22230a
octopus <octopus_truth@163.com>
2020-06-24 14:04:06 +0800

    update

--- app/src/main/java/kim/hsl/rtmp/MainActivity.java
+++ app/src/main/java/kim/hsl/rtmp/MainActivity.java
@@ -166,51 +166,57 @@ public class MainActivity extends AppCompatActivity {
                             Log.i("FrameMetrics", "ANIMATION_DURATION : " +
                                     fm.getMetric(FrameMetrics.ANIMATION_DURATION));
 
-                            // 向 GPU 发送绘制命令花费的时间, 单位纳秒
+                            // 2. 向 GPU 发送绘制命令花费的时间, 单位纳秒
                             Log.i("FrameMetrics", "COMMAND_ISSUE_DURATION : " +
                                     fm.getMetric(FrameMetrics.COMMAND_ISSUE_DURATION));
 
-                            // 将组件树 ( View Hierarchy ) 转为显示列表 ( DisplayLists )
+                            // 3. 将组件树 ( View Hierarchy ) 转为显示列表 ( DisplayLists )
                             // 计算过程所花费的时间, 单位纳秒
                             Log.i("FrameMetrics", "DRAW_DURATION : " +
                                     fm.getMetric(FrameMetrics.DRAW_DURATION));
 
-                            // 绘制的该帧是否是第一帧, 0 是, 1 不是
+                            // 4. 绘制的该帧是否是第一帧, 0 是, 1 不是
                             // 第一帧渲染会慢一些
                             // 第一帧不会引发动画中的跳帧问题, 这些问题都会被窗口动画隐藏
                             // 不必进行显示过程中的 jank 计算
                             Log.i("FrameMetrics", "FIRST_DRAW_FRAME : " +
                                     fm.getMetric(FrameMetrics.FIRST_DRAW_FRAME));
 
-                            // 处理输入事件花费的时间, 单位纳秒
+                            // 5. 处理输入事件花费的时间, 单位纳秒
                             Log.i("FrameMetrics", "INPUT_HANDLING_DURATION : " +
                                     fm.getMetric(FrameMetrics.INPUT_HANDLING_DURATION));
 
-                            // 该值是个时间戳, 表示该帧的 vsync 信号发出时间
+                            // 6. 该值是个时间戳, 表示该帧的 vsync 信号发出时间
                             // 这个时间是当前帧的预期开始时间
                             // 如果该时间与 VSYNC_TIMESTAMP 时间戳不同
                             // 那么说明 UI 线程被阻塞了, 没有及时响应 vsync 信号
                             Log.i("FrameMetrics", "INTENDED_VSYNC_TIMESTAMP : " +
                                     fm.getMetric(FrameMetrics.INTENDED_VSYNC_TIMESTAMP));
 
-                            // 组件树 ( view hierarchy ) 测量 ( measure ) 和摆放 ( layout ) 花费的时间
+                            // 7. 组件树 ( view hierarchy ) 测量 ( measure ) 和摆放 ( layout ) 花费的时间
                             // 单位 纳秒
                             Log.i("FrameMetrics", "LAYOUT_MEASURE_DURATION : " +
                                     fm.getMetric(FrameMetrics.LAYOUT_MEASURE_DURATION));
 
-                            // CPU 传递多维向量图形数据给 GPU 花费的时间, 单位纳秒
+                            // 8. CPU 传递多维向量图形数据给 GPU 花费的时间, 单位纳秒
                             Log.i("FrameMetrics", "SWAP_BUFFERS_DURATION : " +
                                     fm.getMetric(FrameMetrics.SWAP_BUFFERS_DURATION));
 
-                            // 显示列表与显示线程同步花费的时间, 单位纳秒
+                            // 9. 显示列表 ( DisplayLists ) 与显示线程同步花费的时间, 单位纳秒
                             Log.i("FrameMetrics", "SYNC_DURATION : " +
                                     fm.getMetric(FrameMetrics.SYNC_DURATION));
 
-                            //
+                            // 10. CPU 渲染到传递到 GPU 所用的总时间, 上述所花费的有意义的时间之和
+                            // 单位纳秒
                             Log.i("FrameMetrics", "TOTAL_DURATION : " +
                                     fm.getMetric(FrameMetrics.TOTAL_DURATION));
+
+                            // 11. UI 线程响应并开始处理渲染的等待时间, 一般是 0, 如果大于 0 说明出问题了
                             Log.i("FrameMetrics", "UNKNOWN_DELAY_DURATION : " +
                                     fm.getMetric(FrameMetrics.UNKNOWN_DELAY_DURATION));
+
+                            // 12. vsync 信号发出的时间戳, 该时刻 GPU 应该进行绘制, 间隔 16ms
+                            // 同时 CPU 开始渲染
                             Log.i("FrameMetrics", "VSYNC_TIMESTAMP : " +
                                     fm.getMetric(FrameMetrics.VSYNC_TIMESTAMP));
                         }
